934f906894e0c5f81dce274fe419fd2fc4318191
dahl <dahl@DAHL-US.us.oracle.com>
2018-06-28 14:35:46 -0400

    Set PC to reset vector at launch. Display registers at complete. Change JS target to ES2017

--- 6502.js
+++ 6502.js
@@ -1,18 +1,35 @@
 class p6502 {
-    constructor() {
-        this.flags = {
-            carry: false,
-            zero: false,
-            interruptDisable: false,
-            decimalMode: false,
-            breakCmd: false,
-            overflow: false,
-            negative: false //Result of last op had bit 7 set to 1
-        };
+    static boot() {
+        this.loadMemory('mem.hex');
+        this.PC = this.getResetVector();
     }
-    static loadMemory() {
+    static loadMemory(filePath) {
         let fs = require("fs");
-        this.mem = fs.readFileSync('mem.hex');
+        this.mem = fs.readFileSync(filePath);
+    }
+    static getResetVector() {
+        return combineHex(this.mem.slice(0xFFFC, 0xFFFE).reverse());
+    }
+    static displayState() {
+        console.log(`[ACC: 0x${this.ACC.toString(16).padStart(2, "0")} X: 0x${this.X.toString(16).padStart(2, "0")} Y: 0x${this.Y.toString(16).padStart(2, "0")} PC: 0x${this.PC.toString(16).padStart(4, "0")} SP: 0x${this.SP.toString(16).padStart(2, "0")} ]`);
     }
 }
-p6502.loadMemory();
+p6502.ACC = 0; //Accumulator
+p6502.X = 0; //Register X
+p6502.Y = 0; //Register Y
+p6502.PC = 0; //Program Counter
+p6502.SP = 0; //Stack Pointer
+p6502.flags = {
+    carry: false,
+    zero: false,
+    interruptDisable: true,
+    decimalMode: false,
+    breakCmd: false,
+    overflow: false,
+    negative: false //Result of last op had bit 7 set to 1
+};
+function combineHex(buff) {
+    return (buff[0] << 8) | (buff[1]);
+}
+p6502.boot();
+p6502.displayState();
--- 6502.ts
+++ 6502.ts
@@ -1,28 +1,48 @@
 class p6502 {
 
-    private static mem: Buffer;
-    private ACC: number;//Accumulator
-    private X: number;  //Register X
-    private Y: number;  //Register Y
-    private PC: number; //Program Counter
-    private SP: number; //Stack Pointer
-    private flags = {
+    private static mem: Uint8Array;
+    private static ACC: number = 0;//Accumulator
+    private static X: number = 0;  //Register X
+    private static Y: number = 0;  //Register Y
+    private static PC: number = 0; //Program Counter
+    private static SP: number = 0; //Stack Pointer
+    private static flags = {
         carry: false, //Last op caused overflow from bit 7 (or 0) of result
         zero: false, //Result of last op was 0
-        interruptDisable: false, //Processor will ignore interrupts when true
+        interruptDisable: true, //Processor will ignore interrupts when true
         decimalMode: false, //Enables BCD arithmetic (ignored in NES)
         breakCmd: false, //Set when BRK op was executed
         overflow: false, //Arithmetic yielded invalid 2's complement result
         negative: false //Result of last op had bit 7 set to 1
     }
 
-    public static loadMemory() {
-        let fs = require("fs");
-        this.mem = fs.readFileSync('mem.hex');
+    public static boot() {
+        this.loadMemory('mem.hex');
+        this.PC = this.getResetVector();
     }
 
+    private static loadMemory(filePath: string) {
+        let fs = require("fs");
+        this.mem = fs.readFileSync(filePath) as Uint8Array;
+    }
 
+    private static getResetVector(): number{
+        return combineHex(this.mem.slice(0xFFFC,0xFFFE).reverse());
+    }
+
+    public static displayState() {
+        console.log(`[ACC: 0x${this.ACC.toString(16).padStart(2, "0")
+                        } X: 0x${this.X.toString(16).padStart(2, "0")
+                        } Y: 0x${this.Y.toString(16).padStart(2, "0")
+                        } PC: 0x${this.PC.toString(16).padStart(4, "0")
+                        } SP: 0x${this.SP.toString(16).padStart(2, "0")} ]`);
+    }
 
 }
 
-p6502.loadMemory();
+function combineHex(buff: Uint8Array) {
+    return (buff[0]<<8)|(buff[1]);
+}
+
+p6502.boot();
+p6502.displayState();
